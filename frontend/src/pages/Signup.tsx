e32aa91f345bb8c7ffe525aeea7f16b8ed0b235b
vavali08 <vavali@seas.upenn.edu>
2024-04-29 01:49:46 +0000

    fixed create_tables

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -64,7 +64,7 @@ export default function Signup() {
           
                 if (response.status === 200) {
                     // response.render('homepage.ejs', {data: null, comments: null, message: 'Getting friends unsuccessful.', user: user});
-                navigate ("/" + username +"/setProfilePhoto'")
+                navigate ("/" + username +"/setProfilePhoto")
                 //   navigate("/"+ username+"/home");
                 }
                 
