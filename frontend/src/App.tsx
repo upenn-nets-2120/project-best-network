2cf0064efb9827802e26e8f293362cc9afa5d263
cbos558 <cbos@seas.upenn.edu>
2024-05-05 22:19:08 -0400

    fixed stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Friends from "./pages/Friends";
-import ChatInterface from "./pages/ChatInterface";
+import SearchBar from "./pages/SearchBar";
 import ChatPage from "./pages/ChatPage";
 import ProfilePage from './pages/ProfilePage'; 
 import ProfilePhoto from './pages/ProfilePhoto'
@@ -16,7 +16,7 @@ function App() {
         <Route path='/signup' element={<Signup />} />
         <Route path='/:username/home' element={<Home />} />
         <Route path='/:username/friends' element={<Friends />} />
-        <Route path="/:username/chat_nlp" element={<ChatInterface />} />
+        <Route path="/:username/chat_nlp" element={<SearchBar />} />
         <Route path="/:username/chat" element={<ChatPage />} />
         <Route path="/:username/setProfilePhoto" element={<ProfilePhoto />} />
         <Route path="/:username/ProfilePage" element={<ProfilePage />} />
