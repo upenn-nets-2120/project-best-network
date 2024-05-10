100680b7c8b0b29301641b742a487e695e8a7b1a
Julia Susser <57605923+Julia-Susser@users.noreply.github.com>
2024-05-10 19:56:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const config = require('./config.json');
 
 const io = socketIo(server, {
   cors: {
-      origin: "http://localhost:4567", // Specify allowed origins
+      origin: config.ec2+':4567', // Specify allowed origins
       methods: ["GET", "POST"], // Specify allowed methods
       credentials: true // Optional: Allow credentials
   }
@@ -23,7 +23,7 @@ socketHandler.socketHandler(io);
 
 
 app.use(cors({
-  origin: 'http://localhost:4567', // Ensure this matches your front-end URL
+  origin: config.ec2+':4567', // Ensure this matches your front-end URL
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
