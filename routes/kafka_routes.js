5a241dffb09b40b91de123b9dc3ee1b81d785fda
Kimberly Liang <76824780+kimberlyliang@users.noreply.github.com>
2024-05-10 16:07:20 -0400

    fixed

--- routes/kafka_routes.js
+++ routes/kafka_routes.js
@@ -2,7 +2,7 @@ const dbsingleton = require('../models/db_access.js');
 const config = require('../config.json'); 
 const process = require('process');
 const s3Access = require('../models/s3_access.js'); 
-const { sendFederatedPost } = require('../kafka/kafka_app.js'); 
+const { sendFederatedPost } = require('../kafka/updatedapp.js'); 
 
 const handleFederatedPost = async (req, res) => {
     try {
@@ -11,14 +11,14 @@ const handleFederatedPost = async (req, res) => {
         console.log("entered the federatedpost section");
         console.log("attach", attach); 
 
-        // await sendFederatedPost({
-        //     username,
-        //     source_site,
-        //     post_uuid_within_site,
-        //     post_text,
-        //     content_type,
-        //     attach
-        // });
+        await sendFederatedPost({
+            username,
+            source_site,
+            post_uuid_within_site,
+            post_text,
+            content_type,
+            attach
+        });
 
         
         res.status(200).json({ message: 'Federated post sent successfully' });
