1040f9b079409117aa67ff18f7d0ce2f89fc1cfe
Julia Susser <57605923+Julia-Susser@users.noreply.github.com>
2024-05-12 15:42:56 -0400

    comment out face matching loading

--- face-matching/app.js
+++ face-matching/app.js
@@ -179,37 +179,37 @@ initializeFaceModels()
   // START COMMENT BELOW WITH /**
 
   
-  fs.readdir("./face-matching/images", function (err, files) {
-    if (err) {
-      console.error("Could not list the directory.", err);
-      process.exit(1);
-    }
+  // fs.readdir("./face-matching/images", function (err, files) {
+  //   if (err) {
+  //     console.error("Could not list the directory.", err);
+  //     process.exit(1);
+  //   }
 
-    files.forEach(function (file, index) {
-      console.info("Adding task for " + file + " to index.");
-      promises.push(indexAllFaces(path.join("./face-matching/images", file), file, collection));
-    });
-    console.info("Done adding promises, waiting for completion.");
-    Promise.all(promises)
-    .then(async (results) => {
-      console.info("All images indexed.");
+  //   files.forEach(function (file, index) {
+  //     console.info("Adding task for " + file + " to index.");
+  //     promises.push(indexAllFaces(path.join("./face-matching/images", file), file, collection));
+  //   });
+  //   console.info("Done adding promises, waiting for completion.");
+  //   Promise.all(promises)
+  //   .then(async (results) => {
+  //     console.info("All images indexed.");
   
-      // /** 
-      //   const search = 'query.jpg';
+  //     // /** 
+  //     //   const search = 'query.jpg';
     
-      //   console.log('\nTop-k indexed matches to ' + search + ':');
-      //   for (var item of await findTopKMatches(collection, search, 5)) {
-      //     for (var i = 0; i < item.ids[0].length; i++) {
-      //       console.log(item.ids[0][i] + " (Euclidean distance = " + Math.sqrt(item.distances[0][i]) + ") in " + item.documents[0][i]);
-      //     }
-      //   }
-      // ** (/)
+  //     //   console.log('\nTop-k indexed matches to ' + search + ':');
+  //     //   for (var item of await findTopKMatches(collection, search, 5)) {
+  //     //     for (var i = 0; i < item.ids[0].length; i++) {
+  //     //       console.log(item.ids[0][i] + " (Euclidean distance = " + Math.sqrt(item.distances[0][i]) + ") in " + item.documents[0][i]);
+  //     //     }
+  //     //   }
+  //     // ** (/)
     
-    })
-    .catch((err) => {
-      console.error("Error indexing images:", err);
-    });
-    });
+  //   })
+  //   .catch((err) => {
+  //     console.error("Error indexing images:", err);
+  //   });
+  //   });
 
 
     //ADD **/ Above this line for block comment
